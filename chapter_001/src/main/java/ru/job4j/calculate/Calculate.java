bff4e742d4d2b657ad19158590c2f813e4066340
tamlys <oqiwhite1996@gmail.com>
2019-04-28 13:46:59 +0400

    1.3 JavaDoc. Code convention [#138254]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,5 +1,19 @@
 package ru.job4j.calculate;
+
+/**
+*Calculate
+*
+*@author Maxim Sachenkov
+*/
+
 public class Calculate {
+	
+/**
+*main
+*
+*@param args-args.
+*/	
+	
 public static void main(String[] args){
 System.out.println("Hello World");
 	}
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,9 @@
+/**
+*Package for calculate task.
+*
+*@author Maxim Sachenkov (oqiwhite1996@gmail.com)
+*@version $Id$
+*@since 0.1
+*/
+
+package ru.job4j.calculate;
