f016ef034c7b1125e2a7a020023056b12de46143
tamlys <oqiwhite1996@gmail.com>
2019-04-30 23:34:16 +0400

    3.2 Конвертер валюты.[#138239]

---
+++ chapter_001/src/main/java/ru/job4j/converter/Converter.java
@@ -0,0 +1,53 @@
+package ru.job4j.converter;
+
+/**
+ * converter
+ *
+ * @author Maxim Sachenkov (oqiwhite1996@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+
+public class Converter {
+
+    /**
+     * Method rubleToEuro
+     * @param value - is value ruble.
+     * @return convert ruble to euro.
+     */
+
+    public int rubleToEuro(int value) {
+        return value / 70;
+    }
+
+    /**
+     * Method rubleToDollar
+     * @param value - is value ruble.
+     * @return convert ruble to dollar.
+     */
+
+    public int rubleToDollar(int value) {
+        return value / 60;
+    }
+
+    /**
+     * Method euroToRuble
+     * @param value - is value ruble.
+     * @return convert euro to ruble.
+     */
+
+    public int euroToRuble(int value) {
+        return value * 70;
+    }
+
+    /**
+     * Method dollarToRuble
+     * @param value - is value ruble.
+     * @return convert dollar to ruble.
+     */
+
+    public int dollarToRuble(int value) {
+        return value * 60;
+    }
+
+}
---
+++ chapter_001/src/test/java/ru/job4j/converter/ConverterTest.java
@@ -0,0 +1,53 @@
+package ru.job4j.converter;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+/**
+ *  ConverterTest
+ * @author Sachenkov Maxim (oqiwhite1996@gmail.com);
+ * @version $Id$
+ * @since 0.1
+ */
+public class ConverterTest {
+
+    /**
+     * Convert ruble to dollar
+     */
+    @Test
+    public void when60RubleToDollarThen1() {
+        Converter converter = new Converter();
+        int result = converter.rubleToDollar(60);
+        assertThat(result, is(1));
+    }
+
+    /**
+     * Convert ruble to euro
+     */
+    @Test
+    public void when70RubleToEuroThen1() {
+        Converter converter = new Converter();
+        int result = converter.rubleToEuro(70);
+        assertThat(result, is(1));
+    }
+
+    /**
+     * Convert dollar to ruble
+     */
+    @Test
+    public void when1DollarToRubleThen60() {
+        Converter converter = new Converter();
+        int result = converter.dollarToRuble(1);
+        assertThat(result, is(60));
+    }
+
+    /**
+     * Convert euro to ruble
+     */
+    @Test
+    public void when1EuroToRubleThen70() {
+        Converter converter = new Converter();
+        int result = converter.euroToRuble(1);
+        assertThat(result, is(70));
+    }
+}
