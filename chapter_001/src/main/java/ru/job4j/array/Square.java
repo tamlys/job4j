cec5910ae1118e19c5c79b7ab8ee08facdd23a6c
tamlys <oqiwhite1996@gmail.com>
2019-05-08 14:41:58 +0400

    6.0. Заполнить массив степенями чисел.[#138227]

---
+++ chapter_001/src/main/java/ru/job4j/array/Square.java
@@ -0,0 +1,25 @@
+package ru.job4j.array;
+
+/**
+ * Square
+ * @author Maxim Sachenkov (oqiwhite1996@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class Square {
+    /**
+     * calculate
+     * Значение элементов массива начинается с 1
+     * @param bound количество элементов массива
+     * @return возвращает значение элементов массива в квадрате
+     */
+    public int[] calculate(int bound) {
+        int count = 1;
+        int[] rst = new int[bound];
+        for (int i = 0; i != bound; i++) {
+            rst[i] = count * count;
+            count++;
+        }
+        return rst;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/SquareTest.java
@@ -0,0 +1,25 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+/**
+ * Square
+ * @author Maxim Sachenkov (oqiwhite1996@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class SquareTest {
+    /**
+     * whenBound3Then149
+     * Задаем размер массива 3. И возводим каждое значение элементов в квадрат
+     */
+    @Test
+    public void whenBound3Then149() {
+        int bound = 3;
+        Square square = new Square();
+        int[] rst = square.calculate(bound);
+        int[] expect = new int[] {1, 4, 9};
+        assertThat(rst, is(expect));
+    }
+}
