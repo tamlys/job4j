a9110453bd816c8b0b58d5906960236f16bd2372
tamlys <oqiwhite1996@gmail.com>
2019-05-16 14:40:43 +0300

    Сложение двух массивов

---
+++ chapter_001/src/main/java/ru/job4j/Merge.java
@@ -0,0 +1,25 @@
+package ru.job4j;
+
+public class Merge {
+    public int[] merge(int[] left, int[] right) {
+        int firstCount = 0;
+        int secondCount = 0;
+        int[] rsl = new int[left.length + right.length];
+        for (int i = 0; i < rsl.length ; i++) {
+            if (firstCount >= left.length) {
+                rsl[i] = right[secondCount];
+                secondCount++;
+            } else if (secondCount >= right.length) {
+                rsl[i] = left[firstCount];
+                firstCount ++;
+            } else if( left[firstCount] < right[secondCount]){
+                rsl[i] = left[firstCount];
+                firstCount++;
+            } else {
+                rsl[i] = right[secondCount];
+                secondCount++;
+            }
+        }
+        return rsl;
+    }
+}
