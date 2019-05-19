877fcd9ac0f6fff050b76f1312a4d2b5eb10ab84
tamlys <oqiwhite1996@gmail.com>
2019-05-19 22:01:07 +0300

    chapter_002/src/test/java/ru/job4j/condition/Point.java

---
+++ chapter_002/src/main/java/ru/job4j/condition/Point.java
@@ -0,0 +1,15 @@
+package ru.job4j.condition;
+
+import static java.lang.Math.sqrt;
+import static java.lang.Math.pow;
+
+public class Point {
+    private int x;
+    private int y;
+    public double distance(int x1, int y1, int x2, int y2) {
+        return sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+    }
+    public void info() {
+        System.out.println(String.format("Point[%s, %s]", this.x, this.y));
+    }
+}
