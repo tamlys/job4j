ed13b95e0205adff651ca11034a28553866c793c
maxim.sachenkov <oqiwhite1996@gmail.com>
2019-08-08 12:50:33 +0300

    Отсортировать департаменты [#34352]

--- chapter_004/src/main/java/ru/job4j/departament/Departament.java
+++ chapter_004/src/main/java/ru/job4j/departament/Departament.java
@@ -1,10 +1,18 @@
 package ru.job4j.departament;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
+import java.util.*;
+/**
+ * Departament
+ * @author Sachenkov Maxim (oqiwhite1996@gmail.com)
+ * @version $Id$
+ * @since 06.08.19
+ */
 public class Departament {
-    public Set<String> addComponentToFull (List<String> list) {
+    /**
+     * Метод addComponentToFull заполняет список Departament недостающими элементами
+     * @param list список
+     * @return заполненный список
+     */
+    public List<String> addComponentToFull(List<String> list) {
         Set<String> departament = new HashSet<>(list);
             for (String component : list) {
                 if (component.length() == 11 && component.substring(0, 6) != component) {
@@ -17,6 +25,44 @@ public class Departament {
                     }
                 }
             }
-        return departament;
+            List<String> result = new ArrayList<>();
+            result.addAll(departament);
+        return result;
+    }
+    /**
+     * Метод sortUp сортирует список по возрастанию
+     * @param list список
+     * @return отсортированный список
+     */
+    public List<String> sortUp(List<String> list) {
+        List<String> res = new ArrayList<>(list);
+        Collections.sort(res);
+        return res;
+    }
+    /**
+     * Метод sortDown сортирует список по убыванию
+     * @param list список
+     * @return отсортированный список
+     */
+    public List<String> sortDown(List<String> list) {
+        List<String> res = new ArrayList<>(list);
+        Collections.sort(res, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                int result = 0;
+                int length = o1.length() < o2.length() ? o1.length() : o2.length();
+                for (int i = 0; i < length; i++) {
+                    result = Character.compare(o2.charAt(i), o1.charAt(i));
+                    if (result != 0) {
+                        break;
+                    }
+                }
+                if (result == 0) {
+                    result = Integer.compare(o1.length(), o2.length());
+                }
+                return result;
+            }
+        });
+        return res;
     }
 }
--- chapter_004/src/test/java/ru/job4j/departament/DepartamentTest.java
+++ chapter_004/src/test/java/ru/job4j/departament/DepartamentTest.java
@@ -1,17 +1,16 @@
 package ru.job4j.departament;
-
 import org.junit.Test;
-
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
+import java.util.*;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
-
+/**
+ * DepartamentTest
+ * @author Sachenkov Maxim (oqiwhite1996@gmail.com)
+ * @version $Id$
+ * @since 06.08.19
+ */
 public class DepartamentTest {
-@Test
+    @Test
     public void whenAddFullDepartament() {
     Departament departament = new Departament();
     List<String> list = new ArrayList<>();
@@ -22,8 +21,8 @@ public class DepartamentTest {
     list.add("K2");
     list.add("K2/SK1/SSK1");
     list.add("K2/SK1/SSK2");
-    Set<String> result = new HashSet<>();
-    Set<String> expected = new HashSet<>();
+    List<String> result = new ArrayList<>();
+    List<String> expected = new ArrayList<>();
     expected.add("K2/SK1");
     expected.add("K1/SK1");
     expected.add("K1/SK2");
@@ -35,15 +34,57 @@ public class DepartamentTest {
     expected.add("K2/SK1/SSK2");
     result = departament.addComponentToFull(list);
     assertThat(expected, is(result));
-
-
-
-
-
-
-
-
-
-
-}
+    }
+    @Test
+    public void whenDepartamentSortedUp() {
+        Departament departament = new Departament();
+        List<String> list = new ArrayList<>();
+        list.add("K1/SK1");
+        list.add("K1/SK2");
+        list.add("K1/SK1/SSK1");
+        list.add("K1/SK1/SSK2");
+        list.add("K2");
+        list.add("K2/SK1/SSK1");
+        list.add("K2/SK1/SSK2");
+        List<String> result = new ArrayList<>();
+        List<String> expected = new ArrayList<>();
+        expected.add("K1");
+        expected.add("K1/SK1");
+        expected.add("K1/SK1/SSK1");
+        expected.add("K1/SK1/SSK2");
+        expected.add("K1/SK2");
+        expected.add("K2");
+        expected.add("K2/SK1");
+        expected.add("K2/SK1/SSK1");
+        expected.add("K2/SK1/SSK2");
+        result = departament.addComponentToFull(list);
+        result = departament.sortUp(result);
+        assertThat(expected, is(result));
+    }
+    @Test
+    public void whenDepartamentSortedDown() {
+        Departament departament = new Departament();
+        List<String> list = new ArrayList<>();
+        list.add("K1/SK1");
+        list.add("K1/SK2");
+        list.add("K1/SK1/SSK1");
+        list.add("K1/SK1/SSK2");
+        list.add("K2");
+        list.add("K2/SK1/SSK1");
+        list.add("K2/SK1/SSK2");
+        List<String> result = new ArrayList<>();
+        List<String> expected = new ArrayList<>();
+        expected.add("K2");
+        expected.add("K2/SK1");
+        expected.add("K2/SK1/SSK2");
+        expected.add("K2/SK1/SSK1");
+        expected.add("K1");
+        expected.add("K1/SK2");
+        expected.add("K1/SK1");
+        expected.add("K1/SK1/SSK2");
+        expected.add("K1/SK1/SSK1");
+        result = departament.addComponentToFull(list);
+        result = departament.sortDown(result);
+        assertThat(expected, is(result));
+    }
 }
